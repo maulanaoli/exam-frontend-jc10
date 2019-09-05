77e9582ee660d813ec8dd22487a9004d89fffe40
maulana.oli <maulanatazmani@gmail.com>
2019-09-05 19:03:32 +0700

    bagian chart

--- src/1.pages/Cart/Cart.jsx
+++ src/1.pages/Cart/Cart.jsx
@@ -1,243 +1,303 @@
-import React, { Component } from 'react';
-import Axios from 'axios'
-import {connect} from 'react-redux'
-import {urlApi} from '../../3.helpers/database'
-import swal from 'sweetalert'
-import {KartingEuy} from '../../redux/1.actions'
-import {Link} from 'react-router-dom'
+import React, { Component } from "react";
+import Axios from "axios";
+import { connect } from "react-redux";
+import { urlApi } from "../../3.helpers/database";
+import swal from "sweetalert";
+import { KartingEuy } from "../../redux/1.actions";
+import { Link, Redirect } from "react-router-dom";
 
 class Cart extends Component {
-    state = {
-        cartData : [],
-        TotalCheckout: 0,
-        imputPenerima: '',
-        inputKodePos : '',
-        inputAlamat: '',
-        isCheckout: false,
-    }
+  state = {
+    cartData: [],
+    TotalCheckout: 0,
+    imputPenerima: "",
+    inputKodePos: "",
+    inputAlamat: "",
+    isCheckout: false
+  };
 
-    componentWillReceiveProps(newProps){
-        this.getDataCart(newProps.id)
-    }
+  componentWillReceiveProps(newProps) {
+    this.getDataCart(newProps.id);
+  }
 
-    componentDidMount(){
-        this.getDataCart(this.props.id)
-        this.props.KartingEuy(this.props.id)
-       
-    }
-    componentDidUpdate(){
-        this.props.KartingEuy(this.props.id)
-    }
+  componentDidMount() {
+    this.getDataCart(this.props.id);
+    this.props.KartingEuy(this.props.id);
+  }
+  componentDidUpdate() {
+    this.props.KartingEuy(this.props.id);
+  }
 
-    deleteCartItem = (id) => {
-        Axios.delete(urlApi + 'cart/' + id)
-        .then((res) => {
-            swal('Success', 'Item Deleted', 'success')
-            this.getDataCart(this.props.id)
-        })
-        .catch((err) => {
-            swal('Error', 'There is an error', 'error')
-        })
-    }
+  deleteCartItem = id => {
+    Axios.delete(urlApi + "cart/" + id)
+      .then(res => {
+        swal("Success", "Item Deleted", "success");
+        this.getDataCart(this.props.id);
+      })
+      .catch(err => {
+        swal("Error", "There is an error", "error");
+      });
+  };
 
-    getDataCart = (id) => {
-        Axios.get(urlApi + 'cart?userId=' + id)
+  getDataCart = id => {
+    Axios.get(urlApi + "cart?userId=" + id)
+      .then(res => {
+        console.log(res);
+        this.setState({ cartData: res.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  renderCart = () => {
+    var jsx = this.state.cartData.map((val, idx) => {
+      return (
+        <tr>
+          <td>{val.productName}</td>
+          <td>
+            <img src={val.img} height="50px" />
+          </td>
+          <td>{val.price - val.price * (val.discount / 100)}</td>
+          <td>
+            <div className="btn-group">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                onClick={() => this.onBtnEditQty("min", idx)}
+              >
+                -
+              </button>
+              <button type="button" className="btn btn-secondary">
+                {val.quantity}
+              </button>
+              <button
+                type="button"
+                className="btn btn-secondary"
+                onClick={() => this.onBtnEditQty("add", idx)}
+              >
+                +
+              </button>
+            </div>
+          </td>
+          <td>
+            {(val.price - val.price * (val.discount / 100)) * val.quantity}
+          </td>
+          <td>
+            <input
+              type="button"
+              className="btn btn-danger btn-block"
+              onClick={() => this.deleteCartItem(val.id)}
+              value="Delete"
+            />
+          </td>
+        </tr>
+      );
+    });
+
+    return jsx;
+  };
+
+  TotalPrice = () => {
+    let result = 0;
+    this.state.cartData.map(val => {
+      result += val.quantity * (val.price - val.price * (val.discount / 100));
+    });
+    return result;
+  };
+
+  onBtnPay = () => {
+    let TotalPrice = this.TotalPrice();
+    let d = new Date();
+    let today = `${d.getFullYear()}-${d.getMonth() + 1} - ${d.getDate()}`;
+    let items = this.state.cartData;
+    let newData = {
+      userId: this.props.id,
+      items,
+      time: today,
+      TotalPrice,
+      recipient: this.state.inputPenerima,
+      postalCode: this.state.inputKodePos,
+      address: this.state.inputAlamat
+    };
+    if (TotalPrice > this.state.inputUang) {
+      swal("Uang Tidak Cukup", "Uang Anda Tidak Mencukupi", "warning");
+    } else if (this.state.inputUang > TotalPrice) {
+      swal(
+        "Success",
+        "Uang kembalian anda adalah" + (this.state.inputUang - TotalPrice),
+        "success"
+      );
+      Axios.post(urlApi + "history", newData)
         .then(res => {
-            console.log(res)
-            this.setState({cartData : res.data})
+          console.log(res);
+          this.props.KartingEuy(this.props.id);
         })
         .catch(err => {
-            console.log(err)
+          console.log(err);
+        });
+      for (var i = 0; i < this.state.cartData.length; i++) {
+        Axios.delete(urlApi + "cart/" + this.state.cartData[i].id)
+          .then(res => {
+            console.log(res);
+
+            this.getDataCart();
+          })
+          .catch(err => console.log(err));
+      }
+    } else {
+      swal("Success!", "Ntap Gan udah pas uangnya", "Success");
+      Axios.post(urlApi + "history", newData)
+        .then(res => {
+          console.log(res);
         })
+        .catch(err => {
+          console.log(err);
+        });
+      for (var i = 0; i < this.state.cartData.length; i++) {
+        Axios.delete(urlApi + "cart/" + this.state.cartData[i].id)
+          .then(res => {
+            console.log(res);
+            this.getDataCart();
+          })
+          .catch(err => console.log(err));
+      }
     }
+  };
 
-    renderCart = () => {
-        var jsx = this.state.cartData.map((val, idx) => {
-            return (
-                <tr>
-                    <td>{val.productName}</td>
-                    <td><img src={val.img} height='50px'/></td>
-                    <td>{val.price - (val.price * (val.discount/100))}</td>
-                    <td><div className="btn-group">
-                        <button type="button" className="btn btn-secondary" onClick={() => this.onBtnEditQty('min', idx)}>-</button>
-                        <button type="button" className="btn btn-secondary">{val.quantity}</button>
-                        <button type="button" className="btn btn-secondary" onClick={() => this.onBtnEditQty('add', idx)}>+</button>
-                    </div></td>
-                    <td>{(val.price - (val.price * (val.discount/100))) * val.quantity}</td>
-                    <td><input type="button" className="btn btn-danger btn-block" onClick={() => this.deleteCartItem(val.id)} value="Delete"/></td>
-                </tr>
-            )
-        })
+  onBtnEditQty = (action, idx) => {
+    let arrCart = this.state.cartData;
 
-        return jsx
+    if (action == "min") {
+      if (arrCart[idx].quantity > 1) {
+        arrCart[idx].quantity -= 1;
+        Axios.put(urlApi + "cart/" + arrCart[idx].id, arrCart[idx])
+          .then(res => this.getDataCart(this.props.id))
+          .catch(err => console.log(err));
+      }
+    } else if (action == "add") {
+      arrCart[idx].quantity += 1;
+      Axios.put(urlApi + "cart/" + arrCart[idx].id, arrCart[idx])
+        .then(res => this.getDataCart(this.props.id))
+        .catch(err => console.log(err));
     }
+  };
 
-   
-    TotalPrice = () => {
-        let result = 0
-        this.state.cartData.map(val=>{
-            result += val.quantity * (val.price-(val.price * (val.discount/100)))
-        })
-        return result
-    }
+  funCharKosong = () => {
+    alert("chart anda kosong");
+    return <Redirect to="/" />;
+  };
 
-    onBtnPay =()=>{
-        let TotalPrice = this.TotalPrice()
-        let d = new Date()
-        let today = `${d.getFullYear()}-${d.getMonth() + 1} - ${d.getDate()}`
-        let items = this.state.cartData
-        let newData = {
-            userId : this.props.id,
-            items,    
-            time : today,
-            TotalPrice,
-            recipient : this.state.inputPenerima,
-            postalCode : this.state.inputKodePos,
-            address : this.state.inputAlamat
+  render() {
+    return (
+      <div className="container">
+        <table className="table mt-3 text-center">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Gambar</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Total Price</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>{this.renderCart()}</tbody>
 
-        }
-        if (TotalPrice > this.state.inputUang){
-            swal('Uang Tidak Cukup','Uang Anda Tidak Mencukupi' , 'warning')
-
-        }else if (this.state.inputUang > TotalPrice){
-            swal('Success','Uang kembalian anda adalah' + (this.state.inputUang - TotalPrice), 'success')
-            Axios.post(urlApi + 'history', newData)
-            .then(res=>{
-                console.log(res)
-                this.props.KartingEuy(this.props.id)
-                
-            })
-            .catch(err=>{
-                console.log(err)
-            })
-            for(var i = 0; i< this.state.cartData.length; i++){
-                Axios.delete(urlApi + 'cart/' + this.state.cartData[i].id)
-                .then(res=> {
-                    console.log(res)
-                    
-                this.getDataCart()
-                })
-                .catch(err => console.log(err))
-            }
-        } else {
-            swal ('Success!','Ntap Gan udah pas uangnya','Success')
-            Axios.post(urlApi + 'history', newData)
-            .then(res=>{
-                console.log(res)
-                
-            })
-            .catch(err=>{
-                console.log(err)
-            })
-            for(var i = 0; i< this.state.cartData.length; i++){
-                Axios.delete(urlApi + 'cart/' + this.state.cartData[i].id)
-                .then(res=> {
-                    console.log(res)
-                this.getDataCart()})
-                .catch(err => console.log(err))
-            }
-        }
-        }
-    
-    onBtnEditQty = (action, idx) => {
-        let arrCart = this.state.cartData
-
-        if(action == 'min'){
-            if(arrCart[idx].quantity > 1){
-                arrCart[idx].quantity -= 1
-                Axios.put(urlApi + 'cart/' + arrCart[idx].id, arrCart[idx])
-                .then(res => this.getDataCart(this.props.id))
-                .catch(err => console.log(err))
-            }
-        }else if(action == 'add'){
-            arrCart[idx].quantity += 1
-            Axios.put(urlApi + 'cart/' + arrCart[idx].id, arrCart[idx])
-            .then(res => this.getDataCart(this.props.id))
-            .catch(err => console.log(err))
-        }
-    }
-
-    render() {
-        return (
-            <div className="container">
-                <table className="table mt-3 text-center">
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Gambar</th>
-                            <th>Price</th>
-                            <th>Quantity</th>
-                            <th>Total Price</th>
-                            <th>Delete</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.renderCart()}
-                    </tbody>
-                   
-                    <div className="row">
-                        <div className="col-8">
-                        <input type="button" className="btn btn-success" value="CHECKOUT" onClick={()=> this.setState({isCheckout:!this.state.isCheckout})}/>
-                        </div>
-                        {/* {this.TotalPrice()} */}
-                        { 
-                         this.state.cartData.length > 0
-                         ?
-                         <div className="col-4">
-                            <h3>Total Harga = {this.TotalPrice()}</h3>
-                        </div>
-                         :
-                         <div className="col-4">
-                            <h1>Cart Anda Kosong</h1>
-                            <Link to="/"> <input type="button" value="back to menu"/></Link>
-                        </div>
-                        }
-                        
-                        
-                    </div>
-                    {this.state.isCheckout
-                    ?
-                    <div className="row mt-4">
-                        <div className="col-10">
-                            <div className="row">
-                                <div className="col-6">
-                                    <input type="text" onChange={(e)=>{this.setState({inputPenerima: e.target.value})}} className="form-control " placeholder="nama penerima"/>
-                                </div>
-                                <div className="col-6">
-                                    <input type="button" value="pay" onClick={this.onBtnPay} className="btn btn-primary btn-block"/>
-                                </div>
-                            </div>
-                            <div className="row mt-3">
-                                <div className="col-8">
-                                <input type="text" onChange={(e)=>{this.setState({inputAlamat: e.target.value})}} className="form-control " placeholder="Alamat Tujuan"/>
-                                </div>
-                                <div className="col-4">
-                                <input type="text" onChange={(e)=>{this.setState({inputKodePos: e.target.value})}} className="form-control " placeholder="input kode pos"/>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="row mt-3">
-                            <div className="col-12">
-                                <input type="number" onChange={(e)=>{this.setState({inputUang: e.target.value})}} className="form-control" placeholder="Masukkan UanG" />
-                            </div>
-                        </div>
-                    </div>
-                    :
-                    null
-                    }
-                  
-                </table>
+          <div className="row">
+            <div className="col-8">
+              <input
+                type="button"
+                className="btn btn-success"
+                value="CHECKOUT"
+                onClick={() =>
+                  this.setState({ isCheckout: !this.state.isCheckout })
+                }
+              />
             </div>
-        );
-    }
+            {/* {this.TotalPrice()} */}
+            {this.state.cartData.length > 0 ? (
+              <div className="col-8">
+                <h3>Total Harga = {this.TotalPrice()}</h3>
+              </div>
+            ) : (
+              this.funCharKosong()
+            )}
+          </div>
+          {this.state.isCheckout ? (
+            <div className="row mt-4">
+              <div className="col-10">
+                <div className="row">
+                  <div className="col-6">
+                    <input
+                      type="text"
+                      onChange={e => {
+                        this.setState({ inputPenerima: e.target.value });
+                      }}
+                      className="form-control "
+                      placeholder="nama penerima"
+                    />
+                  </div>
+                  <div className="col-6">
+                    <input
+                      type="button"
+                      value="pay"
+                      onClick={this.onBtnPay}
+                      className="btn btn-primary btn-block"
+                    />
+                  </div>
+                </div>
+                <div className="row mt-3">
+                  <div className="col-8">
+                    <input
+                      type="text"
+                      onChange={e => {
+                        this.setState({ inputAlamat: e.target.value });
+                      }}
+                      className="form-control "
+                      placeholder="Alamat Tujuan"
+                    />
+                  </div>
+                  <div className="col-4">
+                    <input
+                      type="text"
+                      onChange={e => {
+                        this.setState({ inputKodePos: e.target.value });
+                      }}
+                      className="form-control "
+                      placeholder="input kode pos"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="row mt-3">
+                <div className="col-12">
+                  <input
+                    type="number"
+                    onChange={e => {
+                      this.setState({ inputUang: e.target.value });
+                    }}
+                    className="form-control"
+                    placeholder="Masukkan UanG"
+                  />
+                </div>
+              </div>
+            </div>
+          ) : null}
+        </table>
+      </div>
+    );
+  }
 }
-
 
 const mapStateToProps = state => {
-    return {
-        id : state.user.id,
-        username : state.user.username
-    }
-}
+  return {
+    id: state.user.id,
+    username: state.user.username
+  };
+};
 
-export default connect(mapStateToProps ,{KartingEuy})(Cart)
+export default connect(
+  mapStateToProps,
+  { KartingEuy }
+)(Cart);
--- src/1.pages/Navbar/Navbar.jsx
+++ src/1.pages/Navbar/Navbar.jsx
@@ -1,120 +1,130 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem } from 'reactstrap';
-import {Link} from 'react-router-dom'
-import {connect} from 'react-redux'
-import Cookie from 'universal-cookie'
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import Cookie from "universal-cookie";
 import { resetUser } from "./../../redux/1.actions";
 
-let cookieObj = new Cookie()
+let cookieObj = new Cookie();
 class NavbarComp extends Component {
-    state = {
-        navbarOpen : false
-    }
+  state = {
+    navbarOpen: false
+  };
 
-    onBtnLogout = () => {
-        cookieObj.remove('userData')
-        this.props.resetUser()
-    }
+  onBtnLogout = () => {
+    cookieObj.remove("userData");
+    this.props.resetUser();
+  };
 
-    render() {
-        return (
-            <div>
-                <Navbar color="light" light expand="md">
-                    <Link to="/"><NavbarBrand>Popokpedia</NavbarBrand></Link>
-                    <NavbarToggler onClick={() => this.setState({navbarOpen : !this.state.navbarOpen})} />
-                    <Collapse navbar>
-                        <Nav className="ml-auto" navbar>
-                            {
-                                this.props.userObj.username !== '' && this.props.userObj.role !== ''
-                                ?
-                                <>
-                                   <NavItem>
-                                       Jumlah Barang {this.props.cart}
-                                   </NavItem>
-                                    <NavItem>
-                                        <NavLink>{this.props.userObj.showId ? this.props.userObj.id : null}</NavLink>
-                                    </NavItem>
-                                    <NavItem>
-                                        <NavLink>{this.props.userObj.username}</NavLink>
-                                    </NavItem>
-                                    <NavItem>
-                                        <NavLink>{this.props.userObj.role}</NavLink>
-                                    </NavItem>
-                                    <UncontrolledDropdown nav inNavbar>
-                                        <DropdownToggle nav caret>
-                                            Options
-                                        </DropdownToggle>
-                                        <DropdownMenu right>
-                                            {
-                                                this.props.userObj.role == 'admin'
-                                                ?
-                                                <>
-                                                <Link style={{textDecoration:'none', color:'inherit'}} to="/admin/dashboard">
-                                                    <DropdownItem>
-                                                        Admin Dashboard
-                                                    </DropdownItem>
-                                                    <DropdownItem onClick={this.onBtnLogout}>
-                                                    Logout
-                                                    </DropdownItem>
-                                                </Link>
-                                                </>
-                                                :
-                                                <>
-                                                <Link to="/cart" style={{textDecoration:'none', color:'inherit'}}>
-                                                <DropdownItem>
-                                                    Cart
-                                                </DropdownItem>
-                                            </Link>
-                                               <Link to = '/history'>
-                                            <DropdownItem>
-                                               History
-                                            </DropdownItem>
-                                               </Link> 
-                                            <DropdownItem divider />
-                                            <DropdownItem onClick={this.onBtnLogout}>
-                                                Logout
-                                            </DropdownItem>
-                                            </>
-                                            }
-                                            </DropdownMenu>
-                                           
-                                    </UncontrolledDropdown>
-                                </>
-                                :
-                                <>
-                                    <NavItem style={{borderRight : '1px solid lightgrey'}}>
-                                        <Link to="/auth"><NavLink>Login</NavLink></Link>
-                                    </NavItem>
-                                    <NavItem>
-                                        <Link to="/auth"><NavLink>Register</NavLink></Link>
-                                    </NavItem>
-                                </>
-                            }
-                        </Nav>
-                    </Collapse>
-                </Navbar>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <Link to="/">
+            <NavbarBrand>Popokpedia</NavbarBrand>
+          </Link>
+          <NavbarToggler
+            onClick={() =>
+              this.setState({ navbarOpen: !this.state.navbarOpen })
+            }
+          />
+          <Collapse navbar>
+            <Nav className="ml-auto" navbar>
+              {this.props.userObj.username !== "" &&
+              this.props.userObj.role !== "" ? (
+                <>
+                  <NavItem>
+                    <NavLink>
+                      {this.props.userObj.showId ? this.props.userObj.id : null}
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink>{this.props.userObj.username}</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink>{this.props.userObj.role}</NavLink>
+                  </NavItem>
+                  <UncontrolledDropdown nav inNavbar>
+                    <DropdownToggle nav caret>
+                      Options
+                    </DropdownToggle>
+                    <DropdownMenu right>
+                      {this.props.userObj.role == "admin" ? (
+                        <>
+                          <Link
+                            style={{ textDecoration: "none", color: "inherit" }}
+                            to="/admin/dashboard"
+                          >
+                            <DropdownItem>Admin Dashboard</DropdownItem>
+                            <DropdownItem onClick={this.onBtnLogout}>
+                              Logout
+                            </DropdownItem>
+                          </Link>
+                        </>
+                      ) : (
+                        <>
+                          <Link
+                            to="/cart"
+                            style={{ textDecoration: "none", color: "inherit" }}
+                          >
+                            <DropdownItem className="coba-coba">
+                              Cart <sup>{this.props.cart}</sup>
+                            </DropdownItem>
+                          </Link>
+                          <Link to="/history">
+                            <DropdownItem>History</DropdownItem>
+                          </Link>
+                          <DropdownItem divider />
+                          <DropdownItem onClick={this.onBtnLogout}>
+                            Logout
+                          </DropdownItem>
+                        </>
+                      )}
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                </>
+              ) : (
+                <>
+                  <NavItem style={{ borderRight: "1px solid lightgrey" }}>
+                    <Link to="/auth">
+                      <NavLink>Login</NavLink>
+                    </Link>
+                  </NavItem>
+                  <NavItem>
+                    <Link to="/auth">
+                      <NavLink>Register</NavLink>
+                    </Link>
+                  </NavItem>
+                </>
+              )}
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = (state) => {
-    return {
-        userObj : state.user,
-        cart : state.cart.cartLength
-        
-    }
-}
+const mapStateToProps = state => {
+  return {
+    userObj: state.user,
+    cart: state.cart.cartLength
+  };
+};
 
-export default connect(mapStateToProps, {resetUser})(NavbarComp)
+export default connect(
+  mapStateToProps,
+  { resetUser }
+)(NavbarComp);
